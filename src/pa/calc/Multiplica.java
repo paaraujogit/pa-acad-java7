1f9350817598fdc63ac7f014f99ad17e80eb4ab2
Daniel <Daniel@pt>
2018-11-09 22:06:02 +0000

    Multiplica

---
+++ src/pa/calc/Multiplica.java
@@ -0,0 +1,14 @@
+
+package pa.calc;
+
+
+public class Multiplica implements ItfOper{
+    
+   public double oper(double op1, double op2){
+
+        return op1 * op2;
+    }  
+   
+      
+    
+}
