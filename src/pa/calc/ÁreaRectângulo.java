628ac454914acdff0447159d266693013e233657
JoaoJac <joaoribeirojacinto@gmail.com>
2018-11-09 21:23:47 +0000

    Adicionada operação de cálculo da área de um rectângulo

---
+++ src/pa/calc/ÁreaRectângulo.java
@@ -0,0 +1,13 @@
+package pa.calc;
+
+public class ÁreaRectângulo implements ItfOper {
+
+	/**
+	 * Calcula a Área de um Rectângulo
+	 */
+	@Override
+	public double oper(double op1, double op2) {
+				
+		return op1 * op2;
+	}
+}
