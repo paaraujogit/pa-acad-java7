458cac556909c56e7ebb292ea50346ff55c5800b
Filipe Coelho <filipemcoelho@gmail.com>
2018-11-10 00:12:33 +0000

    Added calcultor class that implements all interfaces up until this point - minimum implementation

---
+++ src/pa/Calculator/Calculator.java
@@ -0,0 +1,24 @@
+package pa.Calculator;
+
+public class Calculator implements Adder, Subtractor, Multiplicator, Divider {
+
+    public long add(long... operands){
+
+        return 0;
+    }
+
+    public long subtract(long... operands){
+
+        return 0;
+    }
+
+    public long multiply(long... operands){
+
+        return 0;
+    }
+
+    public long divide(long... operands){
+
+        return 0;
+    }
+}
