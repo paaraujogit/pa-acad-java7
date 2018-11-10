dbd0ed75e297f3548c6142235651dedae429c85c
RodolfoFarinha <rodolfo.alves.farinha@gmail.com>
2018-11-10 10:13:14 +0000

    Calculo da Equação de 1º grau Atualizada

--- src/pa/calc/Calculadora.java
+++ src/pa/calc/Calculadora.java
@@ -23,6 +23,15 @@ public class Calculadora {
         Oper03 op03A = new Oper03();
         double res03A = op03A.oper(10, 2);
         System.out.println(res03A);
+        
+        // Eq1grau
+        double a = 5;
+        double b = 2;
+        double c = 1;
+        Eq1grau eq1grau = new Eq1grau();
+        double x1 = eq1grau.x1(a, b, c);
+        double x2 = eq1grau.x2(a, b, c);
+        System.out.println(x1 + " ou " + x2);
 
     }
 }
